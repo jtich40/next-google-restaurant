2ef56d6eb6f69fa42381e37a0901d4b13db07aa0
Jared Tichacek <jared.tichacek@gmail.com>
2024-01-20 23:49:02 -0600

    updated font

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,9 @@
 import Provider from "./Provider";
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 import "./globals.css";
 import HeaderNavbar from "@/components/HeaderNavbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={raleway.className}>
         <Provider>
           <HeaderNavbar />
           {children}
