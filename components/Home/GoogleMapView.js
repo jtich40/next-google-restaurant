2d6305fcb48c709477674b9cf31c2a8bddc53dc1
Jared Tichacek <jared.tichacek@gmail.com>
2024-01-22 18:06:19 -0600

    implemented user location with marker on map

--- components/Home/GoogleMapView.js
+++ components/Home/GoogleMapView.js
@@ -1,5 +1,6 @@
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import React from "react";
+import { UserLocationContext } from "@/context/UserLocationContext";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
+import React, { useContext } from "react";
 
 function GoogleMapView() {
   const { isLoaded } = useJsApiLoader({
@@ -7,19 +8,33 @@ function GoogleMapView() {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
   });
 
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
+
   const containerStyle = {
     width: "100%",
     height: "70vh",
   };
   const coordinate = { lat: -34.397, lng: 150.644 };
+  console.log(userLocation);
   return (
     <div>
       {isLoaded && (
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={coordinate}
+          center={userLocation}
           zoom={13}
-        ></GoogleMap>
+        >
+          <MarkerF
+            position={userLocation}
+            icon={{
+              url: "/user-location.png",
+              scaledSize: {
+                width: 50,
+                height: 50,
+              },
+            }}
+          />
+        </GoogleMap>
       )}
     </div>
   );
